46acf79cf3e065f6369f9cde72b6fafa72e5ca77
Jörg Viola <joerg@joergviola.de>
2012-11-27 08:27:39 +0100

    annotations for doc

--- model/model-scala/app/controllers/Persistent.scala
+++ model/model-scala/app/controllers/Persistent.scala
@@ -9,20 +9,29 @@ import slick.session.Database
 import slick.session.{ Session => SlickSession }
 import slick.driver.ExtendedProfile
 
+// <DriverDefinition>
 trait Persistent {
   def driverClass = Configuration.root().getString("db.default.driver") match {
     case "org.h2.Driver" => "scala.slick.driver.H2Driver"
   }
+  // </DriverDefinition>
 
+  // <DriverClass>
   val driver = singleton[ExtendedProfile](driverClass)
 
   private def singleton[T](name: String)(implicit man: Manifest[T]): T =
     Class.forName(name + "$").getField("MODULE$").get(man.runtimeClass).asInstanceOf[T]
+  // </DriverClass>
 
+  // <DAOs>
   def blogEntries = new BlogEntryDAO(driver).BlogEntries
+  // </DAOs>
 
+  // <DB>
   lazy val db = Database.forDataSource(DB.getDataSource())
+  // </DB>
 
+  // <TransAction>
   def TransAction(f: (Request[AnyContent]) => (SlickSession) => Result): Action[AnyContent] = {
     Action { request =>
       val session = db.createSession
@@ -31,4 +40,5 @@ trait Persistent {
       }
     }
   }
+  // </TransAction>
 }
